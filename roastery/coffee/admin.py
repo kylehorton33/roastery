dc03b432f254f35615aade941d8b9e211b99f925
Kyle Horton <kyle@cplventures.com>
2021-10-25 22:33:11 -0400

    Add Roast and Extraction models - dropped entire db due to migration errors!

--- roastery/coffee/admin.py
+++ roastery/coffee/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Bean
+from .models import Bean, Extraction, Roast
 
 # Register your models here.
 admin.site.register(Bean)
+admin.site.register(Roast)
+admin.site.register(Extraction)
--- roastery/coffee/models.py
+++ roastery/coffee/models.py
@@ -22,5 +22,69 @@ class Bean(TimeStampedModel):
         return self.name
 
     def get_absolute_url(self):
-        """Return absolute URL to the Cheese Detail page."""
+        """Return absolute URL to the Bean Detail page."""
         return reverse("coffee:bean-detail", kwargs={"slug": self.slug})
+
+
+class Roast(TimeStampedModel):
+    class Degree(models.TextChoices):
+        CINNAMON = "cinnamon", "Cinnamon"
+        CITY = "city", "City"
+        CITY_PLUS = "city-plus", "City Plus"
+        FULL_CITY = "full-city", "Full City"
+        VIENNA = "vienna", "Vienna"
+        ESPRESSO = "espresso", "Espresso"
+        ITALIAN = "italian", "Italian"
+
+    green_bean = models.ForeignKey(Bean, verbose_name="Bean", on_delete=models.CASCADE)
+    slug = AutoSlugField(
+        "Roast URL slug", unique=True, always_update=False, populate_from="__str__"
+    )
+    degree = models.CharField("Degree of Roast", max_length=20, choices=Degree.choices)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name="Created by User",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"{self.get_degree_display()} - {self.green_bean.name}"
+
+
+class Extraction(TimeStampedModel):
+    class Method(models.TextChoices):
+        ESPRESSO = "espresso", "Espresso"
+        V60 = "v60", "V60"
+        CHEMEX = "chemex", "Chemex"
+
+    class Grinder(models.TextChoices):
+        NICHE_ZERO = "niche-zero", "Niche Zero"
+        BARATZA_ENCORE = "baratza_encore", "Baratza Encore"
+        HARIO_MINI_MILL = "hario-mini-mill", "Hario Mini Mill"
+
+    roasted_bean = models.ForeignKey(
+        Roast, verbose_name="Roasted Bean", on_delete=models.CASCADE
+    )
+    method = models.CharField(
+        "Method of Extraction",
+        max_length=20,
+        choices=Method.choices,
+        default=Method.ESPRESSO,
+    )
+    grinder = models.CharField(
+        "Grinder", max_length=20, choices=Grinder.choices, default=Grinder.NICHE_ZERO
+    )
+    grind_setting = models.DecimalField(
+        "Grinder Coarseness Setting", decimal_places=1, max_digits=3
+    )
+    temperature = models.DecimalField(
+        "Temperature of Extraction [°C]", decimal_places=1, max_digits=3
+    )
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name="Created by User",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"{self.get_method_display()}: {self.roasted_bean}"
