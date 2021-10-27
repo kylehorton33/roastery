1ec91382356595431d56fb81f26666c4f6468f6c
Kyle Horton <kyle@cplventures.com>
2021-10-26 22:07:30 -0400

    Update and Delete Views for Bean model, modify templates

--- roastery/coffee/urls.py
+++ roastery/coffee/urls.py
@@ -7,8 +7,18 @@ urlpatterns = [
     path(route="beans/", view=views.BeanListView.as_view(), name="bean-list"),
     path(route="beans/add/", view=views.BeanCreateView.as_view(), name="bean-add"),
     path(
-        route="beans/<slug:slug>",
+        route="beans/<slug:slug>/",
         view=views.BeanDetailView.as_view(),
         name="bean-detail",
+    ),
+    path(
+        route="beans/<slug:slug>/update/",
+        view=views.BeanUpdateView.as_view(),
+        name="bean-update",
+    ),
+    path(
+        route="beans/<slug:slug>/delete/",
+        view=views.BeanDeleteView.as_view(),
+        name="bean-delete",
     ),
 ]
--- roastery/coffee/views.py
+++ roastery/coffee/views.py
@@ -1,6 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, DetailView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
 
 from .models import Bean
 
@@ -31,3 +33,16 @@ class BeanCreateView(LoginRequiredWithErrorMessageMixin, CreateView):
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
+
+
+class BeanUpdateView(LoginRequiredWithErrorMessageMixin, UpdateView):
+    model = Bean
+    fields = ["name", "country", "description"]
+    permission_denied_message = "You're not allowed on this page without an account"
+    action = "Update"
+    bootstrap_class = "warning"
+
+
+class BeanDeleteView(LoginRequiredWithErrorMessageMixin, DeleteView):
+    model = Bean
+    success_url = reverse_lazy("coffee:bean-list")
