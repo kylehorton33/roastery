afba6425dc24dfb10577eee74812e81e52ec5f85
Kyle Horton <kyle@cplventures.com>
2021-10-27 17:52:29 -0400

    refactor urls.py

--- roastery/coffee/urls.py
+++ roastery/coffee/urls.py
@@ -1,41 +1,50 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 app_name = "coffee"
-urlpatterns = [
-    path(route="beans/", view=views.BeanListView.as_view(), name="bean-list"),
-    path(route="beans/add/", view=views.BeanCreateView.as_view(), name="bean-add"),
+
+bean_urlpatterns = [
+    path(route="", view=views.BeanListView.as_view(), name="bean-list"),
+    path(route="add/", view=views.BeanCreateView.as_view(), name="bean-add"),
     path(
-        route="beans/<slug:slug>/",
+        route="<slug:slug>/",
         view=views.BeanDetailView.as_view(),
         name="bean-detail",
     ),
     path(
-        route="beans/<slug:slug>/update/",
+        route="<slug:slug>/update/",
         view=views.BeanUpdateView.as_view(),
         name="bean-update",
     ),
     path(
-        route="beans/<slug:slug>/delete/",
+        route="<slug:slug>/delete/",
         view=views.BeanDeleteView.as_view(),
         name="bean-delete",
     ),
-    path(route="roasts/", view=views.RoastListView.as_view(), name="roast-list"),
-    path(route="roasts/add/", view=views.RoastCreateView.as_view(), name="roast-add"),
+]
+
+roast_urlpatterns = [
+    path(route="", view=views.RoastListView.as_view(), name="roast-list"),
+    path(route="add/", view=views.RoastCreateView.as_view(), name="roast-add"),
     path(
-        route="roasts/<slug:slug>/",
+        route="<slug:slug>/",
         view=views.RoastDetailView.as_view(),
         name="roast-detail",
     ),
     path(
-        route="roasts/<slug:slug>/update/",
+        route="<slug:slug>/update/",
         view=views.RoastUpdateView.as_view(),
         name="roast-update",
     ),
     path(
-        route="roasts/<slug:slug>/delete/",
+        route="<slug:slug>/delete/",
         view=views.RoastDeleteView.as_view(),
         name="roast-delete",
     ),
+]
+
+urlpatterns = [
+    path("beans/", include(bean_urlpatterns)),
+    path("roasts/", include(roast_urlpatterns)),
 ]
