2a0a8cb63a0af4f3bff2422d8b86e5bcbc9a6a3d
Kyle Horton <kyle@cplventures.com>
2021-10-26 22:13:50 -0400

    Update tests to cover all new models (still missing form view coverage)

--- roastery/coffee/tests/factories.py
+++ roastery/coffee/tests/factories.py
@@ -4,7 +4,7 @@ from django.template.defaultfilters import slugify
 
 from roastery.users.tests.factories import UserFactory
 
-from ..models import Bean
+from ..models import Bean, Extraction, Roast
 
 
 class BeanFactory(factory.django.DjangoModelFactory):
@@ -15,3 +15,32 @@ class BeanFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Bean
+
+
+ROAST_DEGREE_CHOICES = [x[0] for x in Roast.Degree]
+
+
+class RoastFactory(factory.django.DjangoModelFactory):
+    green_bean = factory.SubFactory(BeanFactory)
+    slug = factory.LazyAttribute(lambda obj: slugify(obj.__str__))
+    degree = factory.fuzzy.FuzzyChoice(ROAST_DEGREE_CHOICES)
+    created_by = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = Roast
+
+
+EXTRACTION_METHOD_CHOICES = [x[0] for x in Extraction.Method]
+EXTRACTION_GRINDER_CHOICES = [x[0] for x in Extraction.Grinder]
+
+
+class ExtractionFactory(factory.django.DjangoModelFactory):
+    roasted_bean = factory.SubFactory(RoastFactory)
+    method = factory.fuzzy.FuzzyChoice(EXTRACTION_METHOD_CHOICES)
+    grinder = factory.fuzzy.FuzzyChoice(EXTRACTION_GRINDER_CHOICES)
+    grind_setting = factory.Faker("pydecimal", left_digits=2, right_digits=1)
+    temperature = factory.Faker("pydecimal", left_digits=2, right_digits=1)
+    created_by = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = Extraction
--- roastery/coffee/tests/test_models.py
+++ roastery/coffee/tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from .factories import BeanFactory
+from .factories import BeanFactory, ExtractionFactory, RoastFactory
 
 pytestmark = pytest.mark.django_db
 
@@ -14,4 +14,22 @@ def test_bean__str__():
 def test_bean_get_absolute_url():
     bean = BeanFactory()
     url = bean.get_absolute_url()
-    assert url == f"/coffee/beans/{bean.slug}"
+    assert url == f"/coffee/beans/{bean.slug}/"
+
+
+def test_roast__str__():
+    roast = RoastFactory()
+    assert roast.__str__() == f"{roast.get_degree_display()} - {roast.green_bean.name}"
+    assert str(roast) == f"{roast.get_degree_display()} - {roast.green_bean.name}"
+
+
+def test_extraction__str__():
+    extraction = ExtractionFactory()
+    assert (
+        extraction.__str__()
+        == f"{extraction.get_method_display()}: {extraction.roasted_bean}"
+    )
+    assert (
+        str(extraction)
+        == f"{extraction.get_method_display()}: {extraction.roasted_bean}"
+    )
