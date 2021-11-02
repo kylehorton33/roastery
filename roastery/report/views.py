287c520d200ba416cbec1022eb1a598ce6dc28c6
Kyle Horton <kyle@cplventures.com>
2021-11-02 18:58:24 -0400

    Add QR code to label

--- roastery/report/views.py
+++ roastery/report/views.py
@@ -1,6 +1,7 @@
 import io
 from datetime import datetime
 
+import qrcode
 from django.http import FileResponse
 from django.http.response import Http404
 from reportlab.lib.units import inch
@@ -25,7 +26,7 @@ def generate_bean_label(request):
     c = canvas.Canvas(buffer, pagesize=pagesize)
 
     text_object = c.beginText()
-    text_object.setTextOrigin(0.2 * inch, 1.8 * inch)
+    text_object.setTextOrigin(0.2 * inch, 1.6 * inch)
     text_object.setFont("Helvetica", 12)
 
     lines = [
@@ -33,10 +34,22 @@ def generate_bean_label(request):
         f"Origin: {bean.country.name}",
     ]
 
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(f"{bean.get_absolute_url()}")
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+
     for line in lines:
         text_object.textLine(line)
 
     c.drawText(text_object)
+
+    c.drawInlineImage(img, 2.4 * inch, 0.5 * inch, 1.3 * inch, 1.3 * inch)
 
     c.showPage()
     c.save()
